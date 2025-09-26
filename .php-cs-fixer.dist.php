d4c150e88bfe2b150db9e5313fb6bf8ea727e954
Víctor de Vierna <victor.vierna@secture.com>
2025-09-26 09:35:49 +0200

    chore(tooling): Add PHP CS Fixer configuration and update composer scripts (#3)

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->in([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->exclude([
+        'var',
+        'vendor',
+    ])
+    ->name('*.php')
+    ->ignoreVCS(true);
+
+return (new Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PhpCsFixer' => true,
+        '@PHP80Migration' => true,
+        '@PHP81Migration' => true,
+        'no_unused_imports' => true,
+        'linebreak_after_opening_tag' => false,
+        'single_quote' => true,
+        'declare_strict_types' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'blank_line_before_statement' => [
+            'statements' => ['return', 'throw', 'try'],
+        ],
+        'global_namespace_import' => [
+            'import_constants' => true,
+            'import_functions' => true,
+        ],
+        'ordered_imports' => [
+            'imports_order' => ['class', 'function', 'const'],
+            'sort_algorithm' => 'alpha',
+        ],
+        'phpdoc_summary' => false,
+        'yoda_style' => false,
+    ])
+    ->setFinder($finder)
+    ->setCacheFile(__DIR__ . '/var/.php-cs-fixer.cache');
