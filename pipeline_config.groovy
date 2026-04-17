c44e177c6fc313e73810b3440be93f1d382d6bc7
Braulio Chávez <braulio.chavez@openpay.mx>
2026-04-16 18:23:48 -0600

    Specification of agents to be used in pipeline execution is added

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -5,7 +5,8 @@ maven_install = 'openpay-maven-3.9.6'
 sonatype_credentials_id = 'ossrh'
 secret_name = 'op-mx-dev-jenkins'
 git_credentials_id = 'jenkins-github-latam-ct'
-
+agent= 'op_jenkins_mx_dev_slave_2023_php'
+init_agent = 'op_jenkins_mx_dev_slave_2023_php'
 jte {
     pipeline_template = "php_library"
 }
